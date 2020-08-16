39a7354ee740c199c941f310f5e6f5edba740630
pluto <961600928@qq.com>
2020-08-16 20:04:53 +0800

    关注、取消关注的开发

---
+++ src/main/java/com/mingwangxin/community/contraller/FollowController.java
@@ -0,0 +1,63 @@
+package com.mingwangxin.community.contraller;
+
+import com.mingwangxin.community.entity.Page;
+import com.mingwangxin.community.entity.User;
+import com.mingwangxin.community.service.FollowService;
+import com.mingwangxin.community.service.UserService;
+import com.mingwangxin.community.util.CommunityConstant;
+import com.mingwangxin.community.util.CommunityUtil;
+import com.mingwangxin.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class FollowController implements CommunityConstant {
+
+    @Autowired
+    private FollowService followService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/follow", method = RequestMethod.POST)
+    @ResponseBody
+    public String follow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        //  如果没登陆，用拦截器做个检查
+        followService.follow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已关注!");
+    }
+
+    @RequestMapping(path = "/unfollow", method = RequestMethod.POST)
+    @ResponseBody
+    public String unfollow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.unfollow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已取消关注!");
+    }
+
+
+    private boolean hasFollowed(int userId) {
+        if (hostHolder.getUser() == null) {
+            return false;
+        }
+
+        return followService.hasFollowed(hostHolder.getUser().getId(), ENTITY_TYPE_USER, userId);
+    }
+
+}
